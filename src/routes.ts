5d1d8dc25654f86f04225b8a359e1938082b1ae7
Arcanjo <marcos.silva.arcanjo2@gmail.com>
2021-02-27 17:10:08 -0300

    Created SurveysUsers' Repository, Controller and route

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import { SendMailController } from './controllers/SendMailController';
 import {SurveysController}  from './controllers/SurveysController';
 import UserController from './controllers/UserController';
 
@@ -6,11 +7,14 @@ const router = Router();
 
 const userController = new UserController();//abordagem diferente do usado em Nlw anteriores. Tira proveito do TS
 const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
 
 router.get("/surveys",surveysController.show);
+router.post("/surveys",surveysController.create);
 
 router.post("/users", userController.create);
-router.post("/surveys",surveysController.create);
+
+router.post("/sendMail", sendMailController.execute);
 
 export {router}; //a instrutora faz exportações dessa forma pois pode auxiliar com o processo de autoimport
 // do vs code, porém é mais uma preferencia pessoal
