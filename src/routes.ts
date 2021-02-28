a0f673af5f00c0014d53b829cca405e5b82c2353
Arcanjo <marcos.silva.arcanjo2@gmail.com>
2021-02-28 16:26:31 -0300

    Created AnswerController

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import { AnswerController } from './controllers/AnswerController';
 import { SendMailController } from './controllers/SendMailController';
 import {SurveysController}  from './controllers/SurveysController';
 import UserController from './controllers/UserController';
@@ -8,6 +9,7 @@ const router = Router();
 const userController = new UserController();//abordagem diferente do usado em Nlw anteriores. Tira proveito do TS
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 router.get("/surveys",surveysController.show);
 router.post("/surveys",surveysController.create);
@@ -15,6 +17,8 @@ router.post("/surveys",surveysController.create);
 router.post("/users", userController.create);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
 
 export {router}; //a instrutora faz exportações dessa forma pois pode auxiliar com o processo de autoimport
 // do vs code, porém é mais uma preferencia pessoal
